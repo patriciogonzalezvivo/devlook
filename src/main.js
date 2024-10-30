28eeec0b328c7fd3f00894fb68f3b4a3ca15c09c
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-10-30 11:14:04 -0400

    adding GLTF workflow

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';  
+
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
 
@@ -12,6 +15,10 @@ import shader_frag from "./glsl/main.frag";
 
 let W = window,
     D = document;
+
+const hdrUrl = 'assets/little_paris_eiffel_tower_2k.hdr';
+const meshUrl = 'assets/dragon.obj';
+// const meshUrl = 'assets/teapot.glb';
 
 let width = W.innerWidth;
 let height = W.innerHeight;
@@ -39,7 +46,7 @@ controls.update();
 const scene = new THREE.Scene();
 
 // Scene: IBL
-glsl_pipeline.setCubemap("assets/little_paris_eiffel_tower_2k.hdr", scene);
+glsl_pipeline.setCubemap(hdrUrl, scene);
 
 // Scene: Lights
 const light = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -69,25 +76,43 @@ const devlook_billboard_0 = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), glsl_p
 devlook_billboard_0.material.transparent = true;
 scene.add(devlook_billboard_0);
 
-// Scene: Mesh
-const objLoader = new OBJLoader();
-objLoader.load(
-    'assets/dragon.obj',
-    function ( object ) { 
-        // Smooth surface
-        object.children[0].geometry.deleteAttribute('normal');
-        object.children[0].geometry = BufferGeometryUtils.mergeVertices(object.children[0].geometry);
-        object.children[0].geometry.computeVertexNormals(object.children[0].geometry);
-        // Set material
-        object.children[0].material = glsl_pipeline.material;
-        object.children[0].castShadow = true;
-        object.children[0].receiveShadow = true;
-        // Add to scene
-        scene.add( object ); 
-    },
-    function ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
-    function ( error ) { console.log( 'An error happened' ); }
-);
+const onProgress = function ( xhr ) { if ( xhr.lengthComputable ) { console.log(xhr.loaded / xhr.total * 100); } };
+const onError = function ( e ) { console.error( e ); };
+
+// Scene: set loader
+var filepathSplit = meshUrl.split('.');
+var fileExt = filepathSplit.pop().toLowerCase();
+var loader = undefined;
+if (fileExt == "obj") {
+    loader = new OBJLoader();
+}
+else if (fileExt == "gltf" || fileExt == "glb") {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( 'https://unpkg.com/three@latest/examples/jsm/libs/draco/gltf/' );
+    loader = new GLTFLoader();
+    loader.setDRACOLoader( dracoLoader );
+}
+
+// Scene: Load the mesh
+loader.load(meshUrl, function ( object ) {
+    const model = object.scene || object;
+    // for each child in the model
+    model.traverse((child) => {
+        if (child.isMesh) {
+            // Smooth surface
+            child.geometry.deleteAttribute('normal');
+            child.geometry = BufferGeometryUtils.mergeVertices(child.geometry);
+            child.geometry.computeVertexNormals(child.geometry);
+            // Set material
+            child.material = glsl_pipeline.material;
+            // Shadows
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    });
+    // Add to scene
+    scene.add( model ); 
+}, onProgress, onError );
 
 const draw = () => {
     glsl_pipeline.renderScene(scene, camera);
